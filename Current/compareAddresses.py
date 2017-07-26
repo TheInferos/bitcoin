4247d0a7bafa840e3a6820c4b41ff708abcaa0d5
Hex C <TheInferos@users.noreply.github.com>
2017-07-26 13:25:25 +0200

    Basic Comparison Complete
    Completed the basic comparision and added in a small upgrade for speed.

--- Current/compareAddresses.py
+++ Current/compareAddresses.py
@@ -1,27 +1,24 @@
 from bitcoin.Current.Objects import Address as addr
 import requests
-
-addr1 = addr.Address("n2BAtxE3FgRPkCbDQbh2WymEwqUCiPB9B7")
-#addr2 = addr.Address("2N672VWCiPhob46jEVrpohd47Xqg7koufug")
-addr2 = "2N672VWCiPhob46jEVrpohd47Xqg7koufug"
-match = False
-
-
-res = requests.get("https://test-insight.bitpay.com/api/tx/" + str(addr1.tx[0])).json()
-#print(res["vin"])
-
-occurance = []
-for transx in addr1.tx:
-    res = requests.get("https://test-insight.bitpay.com/api/tx/" + str(transx)).json()
-    for i in res["vin"]:
-        if "addr" in res["vin"]:
-            if i["addr"] == addr2:
-                match = True
-                occurance.append(addr1.tx)
-    for key in res["vout"]:
-         for hash in (key["scriptPubKey"]["addresses"]):
-             if hash == addr2:
-                match = True
-                occurance.append(addr1.tx)
-
-print (occurance)
+def simpleCompare(addr1Hash, addr2Hash):
+    addr1 = addr.Address(addr1Hash)
+    addr2 = addr.Address(addr2Hash)
+    if len(addr1.tx) <= len(addr2.tx):
+        shorter = addr1
+        compAddr = addr2.hash
+    else:
+        shorter = addr2
+        compAddr = addr1.hash
+    occurance = []
+    for transx in shorter.tx:
+        res = requests.get("https://test-insight.bitpay.com/api/tx/" + str(transx)).json()
+        for i in res["vin"]:
+            if "addr" in res["vin"]:
+                if i["addr"] == compAddr:
+                    occurance.append(transx)
+        for key in res["vout"]:
+             for hash in (key["scriptPubKey"]["addresses"]):
+                 if hash == compAddr:
+                    occurance.append(transx)
+    return occurance
+print(simpleCompare("n2BAtxE3FgRPkCbDQbh2WymEwqUCiPB9B7", "2N672VWCiPhob46jEVrpohd47Xqg7koufug"))
