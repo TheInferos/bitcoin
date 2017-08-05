d653db9f2e2a92970099f6259371bf7cf34700d6
Hex C <TheInferos@users.noreply.github.com>
2017-08-05 16:35:46 +0200

    Updated Namer with lookup
    Updated a name lookup and testing it in main

--- Current/main.py
+++ Current/main.py
@@ -1,4 +1,18 @@
 from bitcoin.Current import namer
-from bitcoin.Current import searchHandler
+from bitcoin.Current import compareAddresses
 
-searchHandler.searchSetup("Address","mmvP3mTe53qxHdPqXEvdu8WdC7GfQ2vmx5")
+address1 = "n2BAtxE3FgRPkCbDQbh2WymEwqUCiPB9B7"
+address2 = "2N672VWCiPhob46jEVrpohd47Xqg7koufug"
+occurance = compareAddresses.simpleCompare( address1, address2)
+names = namer.passNamedAdds()
+address1 = namer.lookupName(address1, names)
+address2 = namer.lookupName(address2, names)
+if occurance != []:
+    message = ""
+    for i in occurance:
+        message += i + " "
+    if len(occurance) == 1:
+        amount = " transaction hash "
+    else:
+        amount = " transaction hashes "
+    print("There is an occurance between " + address1 +" and " + address2 + " at trancation " + amount + message)
--- Current/namer.py
+++ Current/namer.py
@@ -1,25 +1,34 @@
 import os
+
 def passNamedAdds():
-    dict = {}
+    names = {}
     f = open("Docs/NamedAddresses.txt", "r")
     lines = f.read().split("\n")
     for i in range(len(lines)):
         if lines[i] != "":
             row = lines[i].split()
-            dict [row[0]] = row[1]
+            names [row[0]] = row[1]
     f.close()
-    return (dict)
+    return (names)
+
 
 def updatSavedDict(dict):
     f2 = open("Docs/Named2.txt", "a+")
     for k in dict:
         f2.write(k + " " + dict[k] + "\n")
-    readF = f2.read()
     f2.close()
     os.remove("Docs/NamedAddresses.txt")
     os.rename("Docs/Named2.txt", "Docs/NamedAddresses.txt")
+
 
 def addToDict(address, name, dict):
     #add a checker in here
     dict[address] = name
     updatSavedDict()
+
+
+def lookupName(address, names):
+    if address in names:
+        return names[address]
+    else:
+        return address
